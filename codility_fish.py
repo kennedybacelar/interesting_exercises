0e7825c099aaada27538a3480013c0f424defd55
Kennedy <kennedybacelar@yahoo.com.br>
2023-05-21 23:56:22 -0300

    adding solution with stack

--- codility_fish.py
+++ codility_fish.py
@@ -1,4 +1,4 @@
-def solution(A: list, B: list):
+def solution_with_recursion(A: list, B: list):
     """_summary_
     Resolving codility Fish test with recursion
     """
@@ -11,11 +11,35 @@ def solution(A: list, B: list):
             index_eaten_fish = A.index(eaten_fish)
             A.pop(index_eaten_fish)
             B.pop(index_eaten_fish)
-            return solution(A, B)
+            return solution_with_recursion(A, B)
     return len(A)  # Returning the count of fishes alive
 
 
-assert solution([4, 3, 2, 1, 5], [0, 1, 0, 0, 0]) == 2
-assert solution([4, 2, 8, 3, 5, 9, 1], [0, 1, 1, 0, 1, 0, 0]) == 3
-assert solution([4, 5, 6], [0, 0, 1]) == 3
-assert solution([4, 5, 8], [1, 0, 0]) == 2
+def solution_with_stack(A, B):
+    stack_fish_going_right = []
+    survivals = 0
+
+    for fish, direction in zip(A, B):
+        if not direction:  # Fish going left
+            while stack_fish_going_right:
+                if stack_fish_going_right[-1] < fish:
+                    stack_fish_going_right.pop()
+                else:
+                    break
+            else:
+                survivals += 1
+        else:  # Fish going right
+            stack_fish_going_right.append(fish)
+
+    return survivals + len(stack_fish_going_right)
+
+
+assert solution_with_recursion([4, 3, 2, 1, 5], [0, 1, 0, 0, 0]) == 2
+assert solution_with_recursion([4, 2, 8, 3, 5, 9, 1], [0, 1, 1, 0, 1, 0, 0]) == 3
+assert solution_with_recursion([4, 5, 6], [0, 0, 1]) == 3
+assert solution_with_recursion([4, 5, 8], [1, 0, 0]) == 2
+
+assert solution_with_stack([4, 3, 2, 1, 5], [0, 1, 0, 0, 0]) == 2
+assert solution_with_stack([4, 2, 8, 3, 5, 9, 1], [0, 1, 1, 0, 1, 0, 0]) == 3
+assert solution_with_stack([4, 5, 6], [0, 0, 1]) == 3
+assert solution_with_stack([4, 5, 8], [1, 0, 0]) == 2
